a38197b7d1b44020e2482530997f57effa149e75
smuke <smuke@smuke.dev>
2021-11-15 02:20:59 -0800

    Add ready log

--- bot.js
+++ bot.js
@@ -1,1 +1,12 @@
 const config = require("./config.json");
+const { Client, Intents, MessageEmbed } = require("discord.js");
+
+const myIntents = new Intents();
+myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS);
+const client = new Client({ intents: myIntents});
+
+client.once("ready", () => {
+    console.log("Reaction Picker started!")
+})
+
+client.login(config.token);
