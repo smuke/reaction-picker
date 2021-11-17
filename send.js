300546fe71ac78a43514e97c9aa6dd1e3780a740
smuke <smuke@smuke.dev>
2021-11-16 18:17:13 -0800

    Add thumbnail image to winner embed

--- send.js
+++ send.js
@@ -1,24 +1,42 @@
 const { MessageEmbed } = require("discord.js");
 
-exports.sendEmbed = (channel, title, message, fields, footer) => {
+exports.sendEmbed = (channel, title, message, fields, messageID, image) => {
     if (fields != undefined) {
         const embed = new MessageEmbed()
             .setTitle(title)
             .setDescription(message)
             .setFields(fields)
             .setColor("#2a2a2a")
-            .setFooter(footer);
 
-        channel.send({ embeds: [embed] });
+        if (image) {
+            embed.setThumbnail(image);
+        }
+        if (messageID.length > 0) {
+            embed.setTimestamp();
+            embed.setFooter("Msg ID " + messageID);
+            channel.send({ embeds: [embed], reply: { messageReference: messageID } });
+        }
+        else {
+            channel.send({ embeds: [embed] });
+        }
     }
     else {
         const embed = new MessageEmbed()
             .setTitle(title)
             .setDescription(message)
             .setColor("#2a2a2a")
-            .setFooter(footer);
             
-        channel.send({ embeds: [embed] });
+        if (image) {
+            embed.setThumbnail(image);
+        }
+        if (messageID.length > 0) {
+            embed.setTimestamp();
+            embed.setFooter("Msg ID " + messageID);
+            channel.send({ embeds: [embed], reply: { messageReference: messageID } });
+        }
+        else {
+            channel.send({ embeds: [embed] });
+        }
     }
 }
 
